d94ce1919e0cf67955e7c48a30e94ba11bac26bc
NikooBuliaa <nikobulia84@gmail.com>
2025-04-28 12:58:02 +0400

    .help5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { ErrorComponent } from './pages/error/error.component';
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
-    { path: 'about', loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) }, // ✅ ეს დაამატე
+    { path: 'about', loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) }, 
     { path: 'trains', loadComponent: () => import('./components/train-list/train-list.component').then(m => m.TrainListComponent) },
     { path: 'booking', loadComponent: () => import('./pages/booking-page/booking-page.component').then(m => m.BookingPageComponent) },
     { path: 'transaction', loadComponent: () => import('./pages/transaction-page/transaction-page.component').then(m => m.TransactionPageComponent) },
